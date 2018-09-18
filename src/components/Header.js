6e782e84eafebe6459b53d9d7fbe5994512ccffb
Daniel Cender <danielcender@gmail.com>
2018-09-18 10:10:54 -0500

    Add private routes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { startLogout } from '../actions/auth';
 
-export default () => (
+export const Header = ({ startLogout }) => (
 	<header>
 		<h1>Expensify</h1>
 		<NavLink exact to="/" activeClassName="is-active">
@@ -13,5 +15,15 @@ export default () => (
 		<NavLink to="/help" activeClassName="is-active">
 			Help
 		</NavLink>
+		<button onClick={startLogout}>Logout</button>
 	</header>
 );
+
+const mapDispatchToProps = dispatch => ({
+	startLogout: () => dispatch(startLogout()),
+});
+
+export default connect(
+	undefined,
+	mapDispatchToProps,
+)(Header);
--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Header from '../../components/Header';
+import { Header } from '../../components/Header';
+import { start } from 'repl';
+
+let startLogout, wrapper;
+
+beforeEach(() => {
+	startLogout = jest.fn();
+	wrapper = shallow(<Header startLogout={startLogout} />);
+});
 
 test('should render Header correctly', () => {
-	const wrapper = shallow(<Header />);
+	const wrapper = shallow(<Header startLogout={startLogout} />);
 	expect(wrapper).toMatchSnapshot();
 });
+
+test('should call startLogout on button click', () => {
+	wrapper.find('button').simulate('click', {});
+	expect(startLogout).toHaveBeenCalled();
+});
+
+test('LoginPage test file -> should call startLogin on button click', () => {});
